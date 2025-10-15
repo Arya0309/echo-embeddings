099c698b057c5844efdffec2733037e2f8c09f12
Arya0309 <arya59049@gapp.nthu.edu.tw>
2025-10-15 13:06:07 +0800

    update: main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ model = EchoModel(
     "mistralai/Mistral-7B-Instruct-v0.1", templates, pooling_strategy="mean"
 )
 
-tasks = mteb.get_tasks(tasks=["ClimateFEVER"])
+tasks = mteb.get_tasks(tasks=by_type["Classification"][:6])
 
 evaluation = mteb.MTEB(tasks=tasks)
 evaluation.run(
